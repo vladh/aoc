3bc0bc687fb274b9f3acdbc99613bc80d952e670
Vlad-Stefan Harbuz <vlad@vlad.website>
2025-12-01 15:37:48 +0000

    add 2025 day 1 part 1

---
+++ 2025/src/bin/01-1.rs
@@ -0,0 +1,31 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn main() {
+    let mut n_zeros = 0;
+    let mut pos = 50;
+    if let Ok(lines) = read_lines("data/01") {
+        for line in lines.map_while(Result::ok) {
+            let mut chars = line.chars();
+            let dir = chars.next().expect("did not get direction character");
+            let mut amount: i32 = chars.as_str().parse().expect("did not get amount");
+            if dir == 'L' {
+                amount *= -1;
+            }
+            pos += amount;
+            pos = pos.rem_euclid(100);
+            if pos == 0 {
+                n_zeros += 1;
+            }
+            println!("-> {}", pos);
+        }
+    }
+    println!("{}", n_zeros);
+}
